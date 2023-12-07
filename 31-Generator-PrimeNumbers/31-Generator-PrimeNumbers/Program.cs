fecf692f0a2fac1654d09f17673fa8b79ab31ee2
Jcarpenter <Juniorcarpenter00@gmail.com>
2023-12-06 21:11:17 -0400

    31

---
+++ 31-Generator-PrimeNumbers/31-Generator-PrimeNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write($"How much primer number you want: ");
+var quantityPrimeNumbers = int.Parse(Console.ReadLine()!);
+int count = 0;
+long manipulateJ = 0;
+
+for (int i = 2; i > 0; ++i)
+{
+    if (manipulateJ == 0)
+        manipulateJ = 1000000000000000000;
+
+    for (int j = 1; j < manipulateJ; ++j)
+    {
+        if (i % j == 0)
+            count++;
+        if (j > i && count == 2)
+        {
+            Console.WriteLine(i);
+            manipulateJ = 0;
+            --quantityPrimeNumbers;
+            count = 0;
+        }
+        if (count > 2)
+        {
+            count = 0;
+            manipulateJ = 0;
+        }
+    }
+
+    if (quantityPrimeNumbers == 0)
+        break;
+}
+
